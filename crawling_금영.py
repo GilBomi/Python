ce5accb90bce39271221e631a5e806ec0310a356
gilbomi <gil1122@naver.com>
2019-09-26 02:03:02 +0900

    크롤링 완료

--- crawling_금영.py
+++ crawling_금영.py
@@ -1,25 +1,76 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-#html=urlopen("http://www.ikaraoke.kr/isong/search_newsong.asp?page=1&s_date=201908")
 
-html=urlopen("https://www.tjmedia.co.kr/tjsong/song_monthPopular.asp")
-bsObject=BeautifulSoup(html,"html.parser")
+# html=urlopen("http://www.ikaraoke.kr/isong/search_newsong.asp?page=1&s_date=201908")
+
+html = urlopen("http://www.ikaraoke.kr/isong/hit_song.asp")
+bsObject = BeautifulSoup(html, "html.parser")
 # http://www.ikaraoke.kr/isong/search_newsong.asp?page=2&s_date=201908
 # http://www.ikaraoke.kr/isong/search_newsong.asp?page=3&s_date=201908
 book_page_urls = []
-#곡 제목 출력
-# for cover in bsObject.find_all('', {'class','left'}):
-#     #link = cover.get('title')
-#     book_page_urls.append(cover)
+
+# 제목 출력(띄어쓰기 없앰)
+# for cover in bsObject.find_all('a', {'class','b'}):
+#     title = cover.get('title')
+#     arr = str(title).split(' ')
+#     title=''
+#     for i in arr:
+#         title += i
+#     book_page_urls.append(title)
 #
 # for i in book_page_urls:
 #     print(i)
 
-for cover in bsObject.find_all('td'):
-    #link = cover.get('title')
-    book_page_urls.append(cover)
+#가수 출력
+# for cover in bsObject.find_all('td', {'class', 'tit pl8'}):
+#     arr = str(cover).split('>')
+#     singer = str(arr[2]).split('<')[0].split(' ')[0]
+#     book_page_urls.append(singer)
+#
+# for i in book_page_urls:
+#     print(i)
 
-for i in book_page_urls:
-    print(i)
+#곡 번호 출력
+# for cover in bsObject.find_all('td', {'class', 'ac'}):
+#     arr = cover.find_all('input')
+#     for j in arr:
+#         if not len(j):
+#             book_page_urls.append(j.get('value'))
+# for i in book_page_urls:
+#     print(i)
 
 
+
+
+
+
+# 메타 정보로부터 요한 정보를 추출합니다.메타 정보에 없는 저자 정보만 따로 가져왔습니다.
+# for index, book_page_url in enumerate(book_page_urls):
+#     html = urlopen(book_page_url)
+#     bsObject = BeautifulSoup(html, "html.parser")
+#     title = bsObject.find('meta', {'property':'rb:itemName'}).get('content')
+#     author = bsObject.select('span.name a')[0].text
+#     image = bsObject.find('meta', {'property':'rb:itemImage'}).get('content')
+#     url = bsObject.find('meta', {'property':'rb:itemUrl'}).get('content')
+#     originalPrice = bsObject.find('meta', {'property': 'rb:originalPrice'}).get('content')
+#     salePrice = bsObject.find('meta', {'property':'rb:salePrice'}).get('content')
+#     print(index+1, title, author, image, url, originalPrice, salePrice)
+# print(bsObject)
+
+
+# import pymysql
+# db = pymysql.connect(host='localhost',
+#                    port=3306,
+#
+#                    user='root',
+#
+#                    passwd='test123',
+#
+#                     db='test_table',
+#
+#                    charset='utf8')
+# cursor = db.cursor()
+# sql = """INSERT INTO test_table(num, name)
+#         VALUES('test_num', 'test_name');"""
+# cursor.execute(sql)
+# db.commit()
--- crawling_태진.py
+++ crawling_태진.py
@@ -1,72 +1,43 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+#html=urlopen("http://www.tjmedia.co.kr/tjsong/song_search_list.asp")
 
-# html=urlopen("http://www.ikaraoke.kr/isong/search_newsong.asp?page=1&s_date=201908")
-
-html = urlopen("http://www.ikaraoke.kr/isong/hit_song.asp")
-bsObject = BeautifulSoup(html, "html.parser")
+html = urlopen("http://www.tjmedia.co.kr/tjsong/song_search_list.asp?strType=2&strText=%EB%B0%A9%ED%83%84%EC%86%8C%EB%85%84%EB%8B%A8&strCond=0&searchOrderItem=&searchOrderType=")
+bsObject = BeautifulSoup(html,"html.parser")
 # http://www.ikaraoke.kr/isong/search_newsong.asp?page=2&s_date=201908
 # http://www.ikaraoke.kr/isong/search_newsong.asp?page=3&s_date=201908
 book_page_urls = []
 
-# 제목 출력
-for cover in bsObject.find_all('a', {'class','b'}):
-    title = cover.get('title')
-    book_page_urls.append(title)
-
-for i in book_page_urls:
-    print(i)
+#곡 제목 출력(영어제목만 띄어쓰기 되어있고 한글은 안되어있음)
+# for cover in bsObject.find_all('td', {'class', 'left'}):
+#     arr = str(cover).split('>')[1]
+#     title = str(arr).split('<')[0]
+#     book_page_urls.append(title)
+#
+# for i in book_page_urls:
+#     print(i)
 
 #가수 출력
-for cover in bsObject.find_all('td', {'class', 'tit pl8'}):
-    arr = str(cover).split('>')
-    singer = str(arr[2]).split('<')[0].split(' ')[0]
-    book_page_urls.append(singer)
+data = enumerate(bsObject.find_all('td'))
+number = []
+title = []
+singer = []
+for i, cover in data:
+    #link = cover.get('title')
+    if i < 44:
+        continue
+    arr = str(cover).split('>')[1]
+    arr = str(arr).split('<')[0]
+    if len(arr) == 0:
+        continue
+    if ((i-44) % 6) == 0:
+        number.append(arr)
+    if ((i-45) % 6) == 0:
+        title.append(arr)
+    if ((i-46) % 6) == 0:
+        singer.append(arr)
 
-for i in book_page_urls:
+for i in title:
     print(i)
 
-#곡 번호 출력
-for cover in bsObject.find_all('td', {'class', 'ac'}):
-    arr = cover.find_all('input')
-    for j in arr:
-        if not len(j):
-            book_page_urls.append(j.get('value'))
-for i in book_page_urls:
-    print(i)
-
-
-
-
-
-
-# 메타 정보로부터 요한 정보를 추출합니다.메타 정보에 없는 저자 정보만 따로 가져왔습니다.
-# for index, book_page_url in enumerate(book_page_urls):
-#     html = urlopen(book_page_url)
-#     bsObject = BeautifulSoup(html, "html.parser")
-#     title = bsObject.find('meta', {'property':'rb:itemName'}).get('content')
-#     author = bsObject.select('span.name a')[0].text
-#     image = bsObject.find('meta', {'property':'rb:itemImage'}).get('content')
-#     url = bsObject.find('meta', {'property':'rb:itemUrl'}).get('content')
-#     originalPrice = bsObject.find('meta', {'property': 'rb:originalPrice'}).get('content')
-#     salePrice = bsObject.find('meta', {'property':'rb:salePrice'}).get('content')
-#     print(index+1, title, author, image, url, originalPrice, salePrice)
-# print(bsObject)
-
-
-# import pymysql
-# db = pymysql.connect(host='localhost',
-#                    port=3306,
 #
-#                    user='root',
-#
-#                    passwd='test123',
-#
-#                     db='test_table',
-#
-#                    charset='utf8')
-# cursor = db.cursor()
-# sql = """INSERT INTO test_table(num, name)
-#         VALUES('test_num', 'test_name');"""
-# cursor.execute(sql)
-# db.commit()
